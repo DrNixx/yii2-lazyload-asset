3495dbaceed62369979f97a3c2de99729e10aad7
Nick Panteleeff <drnix@chess-online.com>
2018-02-15 19:07:46 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# yii2-lazyload-asset
+Yii2 Assets Bundle for jquery.lazyload library
---
+++ src/assets/LazyLoadAsset.php
@@ -0,0 +1,33 @@
+<?php
+namespace onix\assets;
+
+use yii\web\AssetBundle;
+
+class LazyLoadAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/jquery_lazyload';
+
+    public $css = [
+    ];
+
+    public $js = [
+        'jquery.lazyload.js',
+        'jquery.scrollstop.js',
+    ];
+
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+
+    public function init()
+    {
+        parent::init();
+        $this->publishOptions['beforeCopy'] = function ($from, $to) {
+            if (is_dir($from)) {
+                return false;
+            } else {
+                return pathinfo($from, PATHINFO_EXTENSION) == "js";
+            }
+        };
+    }
+}
